8c04365cd25d0f8fa9ea86363f87aed252ee0178
KhDan007 <kh.dan007@gmail.com>
2023-12-12 22:28:57 +0600

    welcome and ask 5 stats and profile

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ with sqlite3.connect('fitness_bot.db', check_same_thread=False) as conn:
             username TEXT,
             fitness_goal TEXT,
             workout_intensity TEXT,
-            activity_level TEXT
+            activity_level TEXT,
+            weight_preference TEXT,
+            dietary_preference TEXT
         )
     ''')
     conn.commit()
@@ -111,6 +113,14 @@ def handle_activity_level(message):
     # Extract activity level from the user's message
     activity_level = message.text.strip()
 
+    # Ask the user about weight preference
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    weight_preferences = ["With Additional Weight", "Bodyweight Only"]
+    for preference in weight_preferences:
+        markup.add(types.KeyboardButton(preference))
+
+    bot.send_message(user_id, "Do you prefer working with additional weight or with your own weight?", reply_markup=markup)
+
     # Store the activity level in the database
     with sqlite3.connect('fitness_bot.db', check_same_thread=False) as conn:
         cursor = conn.cursor()
@@ -120,15 +130,83 @@ def handle_activity_level(message):
 
         conn.commit()
 
+    # Set user progress to the next step
+    user_progress[user_id] = "weight_preference"
+
+# Handle user's weight preference
+@bot.message_handler(func=lambda message: user_progress.get(message.from_user.id) == "weight_preference")
+def handle_weight_preference(message):
+    user_id = message.from_user.id
+
+    # Extract weight preference from the user's message
+    weight_preference = message.text.strip()
+
+    # Ask the user about dietary preference
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    dietary_preferences = ["Vegetarian", "Vegan", "Gluten-Free", "No Restrictions"]
+    for preference in dietary_preferences:
+        markup.add(types.KeyboardButton(preference))
+
+    bot.send_message(user_id, "Do you have any dietary restrictions or preferences?", reply_markup=markup)
+
+    # Store the weight preference in the database
+    with sqlite3.connect('fitness_bot.db', check_same_thread=False) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE users SET weight_preference = ? WHERE user_id = ?
+        ''', (weight_preference, user_id))
+
+        conn.commit()
+
+    # Set user progress to the next step
+    user_progress[user_id] = "dietary_preference"
+
+# Handle user's dietary preference
+@bot.message_handler(func=lambda message: user_progress.get(message.from_user.id) == "dietary_preference")
+def handle_dietary_preference(message):
+    user_id = message.from_user.id
+
+    # Extract dietary preference from the user's message
+    dietary_preference = message.text.strip()
+
+    # Store the dietary preference in the database
+    with sqlite3.connect('fitness_bot.db', check_same_thread=False) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE users SET dietary_preference = ? WHERE user_id = ?
+        ''', (dietary_preference, user_id))
+
+        conn.commit()
+
     # Thank the user
     bot.send_message(user_id, "You are all set! Here is the list of what this bot can do:")
     bot.send_message(user_id, "1. Choose your fitness goal.")
     bot.send_message(user_id, "2. Choose your workout intensity.")
     bot.send_message(user_id, "3. Choose your activity level.")
-    bot.send_message(user_id, "Thank you! Your fitness goal, workout intensity, and activity level have been recorded.")
+    bot.send_message(user_id, "4. Choose your weight preference.")
+    bot.send_message(user_id, "5. Choose your dietary preference.")
+    bot.send_message(user_id, "Thank you! Your fitness goal, workout intensity, activity level, weight preference, and dietary preference have been recorded.")
 
     # Clear user progress
     del user_progress[user_id]
+
+# Handle the /profile command
+@bot.message_handler(commands=['profile'])
+def handle_profile(message):
+    user_id = message.from_user.id
+
+    # Retrieve user characteristics from the database
+    with sqlite3.connect('fitness_bot.db', check_same_thread=False) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT fitness_goal, workout_intensity, activity_level, weight_preference, dietary_preference FROM users WHERE user_id = ?', (user_id,))
+        result = cursor.fetchone()
+
+    if result:
+        fitness_goal, workout_intensity, activity_level, weight_preference, dietary_preference = result
+        profile_message = f"Your Fitness Profile:\n\nFitness Goal: {fitness_goal}\nWorkout Intensity: {workout_intensity}\nActivity Level: {activity_level}\nWeight Preference: {weight_preference}\nDietary Preference: {dietary_preference}"
+        bot.send_message(user_id, profile_message)
+    else:
+        bot.send_message(user_id, "You haven't set your fitness profile yet. Use the /start command to get started.")
 
 # Start the bot
 print('bot is running')
